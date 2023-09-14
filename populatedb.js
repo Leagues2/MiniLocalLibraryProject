cfb6e4005f68eb4b54b92d291856b4841e914cfb
Leagues2 <wfarid31@gmail.com>
2023-09-14 08:40:23 +0300

    init Controller

--- populatedb.js
+++ populatedb.js
@@ -5,7 +5,7 @@ console.log(
   );
   
   // Get arguments passed on command line
-  const userArgs = process.argv.slice(2);
+  const userArgs = process.argv.slice(2); 
   
   const Book = require("./models/book");
   const Author = require("./models/author");
